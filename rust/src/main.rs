4ecaa04aeb9fca72677efe710a3e3a43366e310a
Thomas Lent <tlent@pm.me>
2024-03-05 14:04:43 -0500

    Optimize

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -35,6 +35,12 @@ const TESTCASES: [&str; 24] = [
     "s-rg-118-30",
 ];
 
+#[derive(Debug, Clone)]
+struct Set {
+    id: usize,
+    elements: BitVec<usize>,
+}
+
 fn main() -> Result<()> {
     let testcases_path = Path::new("../testcases");
     let output_path = Path::new("output");
@@ -89,15 +95,15 @@ fn run_testcase(path: &Path) -> Result<(f64, String)> {
     let element_count: usize = lines.next().expect("Invalid file content").parse()?;
     let set_count: usize = lines.next().expect("Invalid file content").parse()?;
     let elements = bitvec!(usize, Lsb0; 1; element_count);
-    let sets: Vec<(usize, BitVec<usize>)> = (1..)
-        .zip(lines)
-        .map(|(i, line)| {
-            let mut set = bitvec!(usize, Lsb0; 0; element_count);
+    let sets: Vec<Set> = lines
+        .enumerate()
+        .map(|(id, line)| {
+            let mut elements = bitvec!(usize, Lsb0; 0; element_count);
             for s in line.split_ascii_whitespace() {
                 let element: usize = s.parse()?;
-                set.set(element - 1, true);
+                elements.set(element - 1, true);
             }
-            Ok((i, set))
+            Ok(Set { id, elements })
         })
         .collect::<Result<_, ParseIntError>>()?;
     assert_eq!(set_count, sets.len());
@@ -112,27 +118,24 @@ fn run_testcase(path: &Path) -> Result<(f64, String)> {
         Included sets: {:?}\n",
         set_cover.len(),
         format_runtime(runtime),
-        set_cover
+        set_cover.iter().map(|i| i + 1).collect::<Vec<_>>()
     );
     for i in set_cover {
-        let set_elements: Vec<_> = sets[i - 1].1.iter_ones().map(|i| i + 1).collect();
-        output.push_str(&format!("Set #{i}: {:?}\n", set_elements));
+        let set_elements: Vec<_> = sets[i].elements.iter_ones().map(|i| i + 1).collect();
+        let set_number = i + 1;
+        output.push_str(&format!("Set #{set_number}: {:?}\n", set_elements));
     }
     println!("{}", output);
     Ok((runtime, output))
 }
 
-fn find_set_cover(
-    mut sets: Vec<(usize, BitVec<usize>)>,
-    mut uncovered_elements: BitVec<usize>,
-) -> Option<Vec<usize>> {
+fn find_set_cover(mut sets: Vec<Set>, mut uncovered_elements: BitVec<usize>) -> Option<Vec<usize>> {
     if uncovered_elements.count_ones() == 0 {
         return Some(Vec::new());
     }
-    for (index, (i, set)) in sets.iter().enumerate() {
-        for (j, other) in &sets {
-            if i != j && set.iter_ones().all(|i| other[i]) {
-                let mut sets = sets.to_owned();
+    for (index, set) in sets.iter().enumerate() {
+        for other in sets.iter() {
+            if set.id != other.id && set.elements.iter_ones().all(|i| other.elements[i]) {
                 sets.remove(index);
                 return find_set_cover(sets, uncovered_elements);
             }
@@ -141,8 +144,8 @@ fn find_set_cover(
     for element in uncovered_elements.iter_ones() {
         let mut containing_set_count = 0;
         let mut containing_index = 0;
-        for (index, (_, set)) in sets.iter().enumerate() {
-            if set[element] {
+        for (index, set) in sets.iter().enumerate() {
+            if set.elements[element] {
                 containing_set_count += 1;
                 containing_index = index;
                 if containing_set_count > 1 {
@@ -153,45 +156,43 @@ fn find_set_cover(
         if containing_set_count == 0 {
             return None;
         } else if containing_set_count == 1 {
-            let mut new_sets = sets.to_owned();
-            let (required_set_id, required_set) = new_sets.remove(containing_index);
-            for (_, set) in new_sets.iter_mut() {
-                for element in required_set.iter_ones() {
-                    set.set(element, false);
+            let required_set = sets.remove(containing_index);
+            for element in required_set.elements.iter_ones() {
+                uncovered_elements.set(element, false);
+                for set in sets.iter_mut() {
+                    set.elements.set(element, false);
                 }
             }
-            new_sets.retain(|(_, set)| !set.is_empty());
-            for element in required_set.iter_ones() {
-                uncovered_elements.set(element, false);
-            }
-            let mut cover = find_set_cover(new_sets, uncovered_elements);
-            if let Some(set) = cover.as_mut() {
-                set.push(required_set_id);
+            sets.retain(|set| !set.elements.is_empty());
+            let mut cover = find_set_cover(sets, uncovered_elements);
+            if let Some(list) = cover.as_mut() {
+                list.push(required_set.id);
             }
             return cover;
         }
     }
     let mut largest_set_index = 0;
-    for (index, (_, set)) in sets.iter().enumerate() {
-        if set.count_ones() > sets[largest_set_index].1.count_ones() {
+    let mut largest_set_size = 0;
+    for (index, set) in sets.iter().enumerate() {
+        let set_size = set.elements.count_ones();
+        if set_size > largest_set_size {
             largest_set_index = index;
+            largest_set_size = set_size;
         }
     }
-    let (selected_set_id, selected_set) = sets.remove(largest_set_index);
+    let selected_set = sets.remove(largest_set_index);
     let mut new_sets = sets.clone();
     let mut new_uncovered_elements = uncovered_elements.clone();
-    for (_, set) in new_sets.iter_mut() {
-        for element in selected_set.iter_ones() {
-            set.set(element, false);
+    for element in selected_set.elements.iter_ones() {
+        new_uncovered_elements.set(element, false);
+        for set in new_sets.iter_mut() {
+            set.elements.set(element, false);
         }
     }
-    new_sets.retain(|(_, set)| !set.is_empty());
-    for element in selected_set.iter_ones() {
-        new_uncovered_elements.set(element, false);
-    }
+    new_sets.retain(|set| !set.elements.is_empty());
     let mut with_selected = find_set_cover(new_sets, new_uncovered_elements);
-    if let Some(set) = with_selected.as_mut() {
-        set.push(selected_set_id);
+    if let Some(list) = with_selected.as_mut() {
+        list.push(selected_set.id);
     }
     let without_selected = find_set_cover(sets, uncovered_elements);
     match (with_selected, without_selected) {
