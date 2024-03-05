e8722a35b5ebfef421d0725c56b5106097f9320e
Thomas Lent <tlent@pm.me>
2024-03-05 11:49:37 -0500

    Better output

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,14 +1,76 @@
 use anyhow::Result;
 use bitvec::prelude::*;
 use bitvec::vec::BitVec;
-use std::fs::File;
+use std::fs::{self, File};
 use std::io::prelude::*;
 use std::num::ParseIntError;
+use std::path::{Path, PathBuf};
 use std::time::Instant;
 
+const TESTCASES: [&str; 24] = [
+    "s-k-40-60",
+    "s-k-20-30",
+    "s-rg-197-45",
+    "s-k-20-35",
+    "s-rg-733-100",
+    "s-k-35-65",
+    "s-k-30-55",
+    "s-rg-413-75",
+    "s-rg-63-25",
+    "s-rg-31-15",
+    "s-rg-109-35",
+    "s-rg-245-50",
+    "s-k-30-50",
+    "s-k-100-175",
+    "s-k-150-225",
+    // "s-k-150-250",
+    "s-k-200-300",
+    "s-rg-8-10",
+    "s-rg-40-20",
+    "s-k-50-100",
+    "s-k-40-80",
+    "s-rg-155-40",
+    "s-X-12-6",
+    "s-k-50-95",
+    "s-rg-118-30",
+];
+
 fn main() -> Result<()> {
-    let filename = std::env::args().nth(1).expect("Missing filename arg");
-    let mut file = File::open(filename)?;
+    let testcases_path = Path::new("../testcases");
+    let output_path = Path::new("output");
+    let mut output_summary = File::create(output_path.join("summary").with_extension("md"))?;
+    writeln!(
+        &mut output_summary,
+        "|     Testcase     |  C Runtime  | Rust Runtime |  Change  |\n\
+         |------------------|-------------|--------------|----------|"
+    )?;
+    for &testcase in &TESTCASES {
+        let testcase_path = testcases_path.join(testcase).with_extension("txt");
+        let (rust_runtime, rust_output) = run_testcase(&testcase_path)?;
+        let mut output_file = File::create(output_path.join(testcase).with_extension("txt"))?;
+        output_file.write_all(rust_output.as_bytes())?;
+        let c_path = PathBuf::from("../c/output")
+            .join(testcase)
+            .with_extension("txt");
+        let c_output = fs::read_to_string(c_path)?;
+        let line = c_output.lines().next().unwrap();
+        let c_runtime: f64 = line.split_ascii_whitespace().nth(8).unwrap().parse()?;
+        writeln!(
+            &mut output_summary,
+            "| {:^16} | {:>11} | {:>12} | {:>7.2}% |",
+            testcase,
+            format_runtime(c_runtime),
+            format_runtime(rust_runtime),
+            ((rust_runtime - c_runtime) / c_runtime) * 100.0
+        )?;
+    }
+    Ok(())
+}
+
+fn run_testcase(path: &Path) -> Result<(f64, String)> {
+    let testcase_name = path.file_name().unwrap().to_string_lossy();
+    println!("Running testcase {testcase_name}");
+    let mut file = File::open(path)?;
     let mut content = String::new();
     file.read_to_string(&mut content)?;
     let mut lines = content.lines();
@@ -27,22 +89,25 @@ fn main() -> Result<()> {
         })
         .collect::<Result<_, ParseIntError>>()?;
     assert_eq!(set_count, sets.len());
+    let sets_clone = sets.clone();
     let start = Instant::now();
     let mut set_cover =
-        find_set_cover(sets.clone(), elements).expect("Failed to find valid set cover");
-    let end = start.elapsed();
-    println!(
-        "Found minimum set cover containing {} sets in {:.6} seconds.",
-        set_cover.len(),
-        end.as_secs_f64()
-    );
+        find_set_cover(sets_clone, elements).expect("Failed to find valid set cover");
+    let runtime = start.elapsed().as_secs_f64();
     set_cover.sort_unstable();
-    println!("Included sets: {:?}", set_cover);
+    let mut output = format!(
+        "Found minimum set cover containing {} sets in {}.\n\
+        Included sets: {:?}\n",
+        set_cover.len(),
+        format_runtime(runtime),
+        set_cover
+    );
     for i in set_cover {
         let set_elements: Vec<_> = sets[i - 1].1.iter_ones().map(|i| i + 1).collect();
-        println!("Set #{i}: {:?}", set_elements);
+        output.push_str(&format!("Set #{i}: {:?}\n", set_elements));
     }
-    Ok(())
+    println!("{}", output);
+    Ok((runtime, output))
 }
 
 fn find_set_cover(
@@ -122,5 +187,15 @@ fn find_set_cover(
         (None, without @ Some(_)) => without,
         (with @ Some(_), None) => with,
         (Some(with), Some(without)) => Some(std::cmp::min_by_key(with, without, Vec::len)),
+    }
+}
+
+fn format_runtime(runtime: f64) -> String {
+    if runtime < 1e-3 {
+        format!("{:.2} us", runtime * 1e6)
+    } else if runtime < 1.0 {
+        format!("{:.2} ms", runtime * 1e3)
+    } else {
+        format!("{:.2} s", runtime)
     }
 }
